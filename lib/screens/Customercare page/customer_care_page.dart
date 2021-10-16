8ddca00c5c04e6fa7cac391c03764d05a9d27c81
jatin kesharwani <jatinkesharwani360.com>
2021-10-16 16:55:58 +0530

    refactor: change positioned to rows and column

--- lib/screens/Customercare page/customer_care_page.dart
+++ lib/screens/Customercare page/customer_care_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vvplus_app/components/colors.dart';
+import 'package:vvplus_app/components/text_feild.dart';
+import 'package:vvplus_app/components/widgets.dart';
 import 'package:vvplus_app/utilities/rounded_button.dart';
 
 class CustomerCarePage extends StatefulWidget{
@@ -12,46 +14,38 @@ class _CustomerCarePageState extends State<CustomerCarePage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Customer Care ",
-          style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.w600),
-          textAlign: TextAlign.center,
-        ),
-    toolbarHeight: 107,
-    titleTextStyle: TextStyle(color: PrimaryColor3),
-    backgroundColor: PrimaryColor1,
-    ),
-      body: Container(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Positioned(
-              top: 40,
-              child: RoundedButton2(
-                text: "Make a new complaint",
-                press: () {},
+        appBar: appBarMain(appText1),
+      body: SafeArea(
+        child: Container(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RoundedButton2(
+                        text: text15,
+                        press: () {},
+                      ),
+
+                    RoundedButton2(
+                        text: text16,
+                        press: () {
+                          Navigator.push( context, MaterialPageRoute(
+                              builder: (context) => CustomerCarePage()));
+                        },
+                      ),
+                     RoundedButton2(
+                        text: text17,
+                        press: () {},
+                      ),
+                  ],
+                ),
               ),
-            ),
-            Positioned(
-              top: 200,
-              child: RoundedButton2(
-                text: "See progress of previous complaints",
-                press: () {
-                  Navigator.push( context, MaterialPageRoute(
-                      builder: (context) => CustomerCarePage()));
-                },
-              ),
-            ),
-            Positioned(
-              top: 390,
-              child: RoundedButton2(
-                text: "Make a complaint against an employee",
-                press: () {},
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
