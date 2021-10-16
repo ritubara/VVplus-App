9d851c705a5d2f5a3ef01c33f7efb32df4eaf0bb
jatin kesharwani <jatinkesharwani360.com>
2021-10-16 16:47:30 +0530

    refactor: add text

--- lib/components/text_feild.dart
+++ lib/components/text_feild.dart
@@ -1,3 +1,8 @@
+// for App Bar Text
+
+final String appText1 = "Customer Care";
+
+
 // for login page
 
 final String text1 = "Login";
@@ -19,3 +24,9 @@ final String text11 = "Customer Care";
 final String text12 = "Edit Profile";
 final String text13 = "View House";
 final String text14 = "Maintanance";
+
+// for Customer Care Page
+
+final String text15 = "Make a new complaint";
+final String text16 = "See progress of previous complaints";
+final String text17 = "Make a complaint against an employee";
