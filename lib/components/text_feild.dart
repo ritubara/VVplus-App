bd22a63765c1dd51948c4d453102088657e4c3a7
jatin kesharwani <jatinkesharwani360.com>
2021-10-16 11:10:14 +0530

    chore: adding new files

---
+++ lib/components/text_feild.dart
@@ -0,0 +1,7 @@
+final String text1 = "Login";
+final String text2 = "Let's explore your dream house";
+final String text3 = "You will receive an SMS verification that may apply message";
+final String text4 = "and data rates.";
+final String text5 = "Send OTP";
+final String hintText1 = "Enter Mobile Number";
+final String hintText2 = "Enter OTP";
