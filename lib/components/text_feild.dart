5fc825de91827397d1a7d197081f5e0920190b3f
jatin kesharwani <jatinkesharwani360.com>
2021-10-16 16:30:56 +0530

    refactor: adding text string

--- lib/components/text_feild.dart
+++ lib/components/text_feild.dart
@@ -1,3 +1,5 @@
+// for login page
+
 final String text1 = "Login";
 final String text2 = "Let's explore your dream house";
 final String text3 = "You will receive an SMS verification that may apply message";
@@ -5,3 +7,15 @@ final String text4 = "and data rates.";
 final String text5 = "Send OTP";
 final String hintText1 = "Enter Mobile Number";
 final String hintText2 = "Enter OTP";
+
+// for home page
+
+final String text6 = "Notification";
+final String text7 = "Please check your latest house progress";
+final String text8 = "updated by Eng. Devanand.";
+final String text9 = "Verify Construction Status";
+final String text10 = "Progress";
+final String text11 = "Customer Care";
+final String text12 = "Edit Profile";
+final String text13 = "View House";
+final String text14 = "Maintanance";
