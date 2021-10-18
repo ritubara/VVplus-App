9b59bf0c681ab342d25835bd5890e205d237ba44
jatin kesharwani <jatinkesharwani360.com>
2021-10-18 16:23:35 +0530

    chore: add images

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,2 +1,9 @@
-const String image2 = "assets/images/+.png";     // for + sign
-const String image1 = "assets/images/4feyuv9I 1.png";   // for vasthu bihar image
+//  vasthu bihar image
+
+const String image2 = "assets/images/+.png";     //  + sign
+const String image1 = "assets/images/4feyuv9I 1.png";  //  app logo
+
+//  for New complaint screen
+
+const String image3 = "assets/images/Arrow 1.png";
+const String image4 = "assets/images/Rectangle 7.png";
