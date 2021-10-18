a675eea24256ef890e5cb256786924c9b44c8d82
Roshan Mathews <roshanmathewsofficial@gmail.com>
2021-10-18 19:07:12 +0530

    refactor: textstyles,carousel

--- lib/utilities/bottom_navbar.dart
+++ lib/utilities/bottom_navbar.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:vvplus_app/constants/colors.dart';
 
-class BottomNavBar extends StatelessWidget{
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({Key key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _MyBottomNavbar();
+}
+class _MyBottomNavbar extends State<BottomNavBar>{
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +27,24 @@ class BottomNavBar extends StatelessWidget{
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
             backgroundColor: PrimaryColor3,
-            icon: Icon(Icons.circle, color: PrimaryColor5),
+            icon: Icon(Icons.circle),
             title: Text('', style: TextStyle(color: PrimaryColor2))),
         BottomNavigationBarItem(
-            icon: Icon(Icons.circle, color: PrimaryColor5),
+            icon: Icon(Icons.circle),
             title: Text('', style: TextStyle(color: PrimaryColor2))),
         BottomNavigationBarItem(
-            icon: Icon(Icons.circle, color: PrimaryColor5),
+            icon: Icon(Icons.circle),
             title: Text('', style: TextStyle(color: Colors.black))),
         BottomNavigationBarItem(
-            icon: Icon(Icons.circle, color: PrimaryColor5),
+            icon: Icon(Icons.circle),
             title: Text('', style: TextStyle(color: PrimaryColor2))),
       ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.green,
+      onTap: _onItemTapped,
     );
   }
+  }
 
-}
+
+
