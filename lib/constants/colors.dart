03152156235bcac54f2d1d1120cfeeea281b6041
jatin kesharwani <jatinkesharwani360.com>
2021-10-19 18:38:59 +0530

    chore: colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -6,6 +6,8 @@ const PrimaryColor2 = Colors.black;   // black
 const PrimaryColor3 = Colors.white;    // white
 const PrimaryColor4 = Colors.black45;   // black45
 const PrimaryColor5 = Colors.grey;
+const PrimaryColor6 = Colors.red;
+const PrimaryColor7 = Colors.lightGreen;
 
 const TextColor1= Colors.white;
 const TextColor2= Colors.green;
@@ -13,3 +15,4 @@ const TextColor3= Colors.red;
 const TextColor4= Colors.black;
 
 const StepperColor= Colors.purple;
+const StepperColor1 = Colors.orange;
