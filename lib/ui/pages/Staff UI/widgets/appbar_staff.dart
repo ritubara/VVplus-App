06bdeb3970ffa941b19b707707e57f9167ff582f
Roshan Mathews <roshanmathewsofficial@gmail.com>
2021-10-22 17:50:05 +0530

    chore: add staff appbar

---
+++ lib/ui/pages/Staff UI/widgets/appbar_staff.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:vvplus_app/ui/widgets/constants/colors.dart';
+import 'dart:math'as math;
+
+AppBar AppBarSatff(String appBarText) {
+  return AppBar(
+    centerTitle: true,
+    title: Text(
+      appBarText,
+      style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+      textAlign: TextAlign.center,
+    ),
+    actions: <Widget>[
+      Transform(
+        transform: Matrix4.rotationY(math.pi),
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.call,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ],
+    titleTextStyle: const TextStyle(color: PrimaryColor3),
+    backgroundColor: PrimaryColor1,
+  );
+}
