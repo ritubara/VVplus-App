8baa557df92c1a1d122327ce9866ebafa3c74cce
Roshan Mathews <roshanmathewsofficial@gmail.com>
2021-10-23 16:54:44 +0530

    refactor: add Text input

--- lib/ui/pages/Staff UI/widgets/form_text.dart
+++ lib/ui/pages/Staff UI/widgets/form_text.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 
-class TextForm extends StatelessWidget{
-  final String text;
-
-  const TextForm({
-    Key key,
-    this.text,
-}): super(key:key);
-  @override
-  Widget build(BuildContext context) {
-   return Text(
-     text,
-       style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-   );
-  }
+Padding FormsHeadText(String text){
+  return Padding(
+    padding:EdgeInsets.symmetric(horizontal:40,vertical: 10),
+    child: Text(
+      text,
+      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),
+    ),
+  );
 
 }
