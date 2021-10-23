7a28de03fd5a6efdcfd80011dd466447e61a5a9f
Roshan Mathews <roshanmathewsofficial@gmail.com>
2021-10-23 20:16:33 +0530

    refactor: add dropdown form

--- lib/ui/pages/Staff UI/screens/purchase page/Material Request Entry/material_entrt_body.dar.dart
+++ lib/ui/pages/Staff UI/screens/purchase page/Material Request Entry/material_entrt_body.dar.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vvplus_app/ui/pages/Customer%20UI/screens/NewComplaint%20page/drop_down_menu_items.dart';
 import 'package:vvplus_app/ui/pages/Customer%20UI/widgets/decoration_widget.dart';
 import 'package:vvplus_app/ui/pages/Customer%20UI/widgets/text_style_widget.dart';
+import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/form_text.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/staff_decorations.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/text_form_field.dart';
 import 'package:vvplus_app/ui/widgets/Utilities/raisedbutton_text.dart';
@@ -14,6 +16,7 @@ class MaterialEntryBody extends StatefulWidget{
 
 
 class myMaterialEntryBody extends State<MaterialEntryBody>{
+  int valueChoose=4;
 
   @override
   Widget build(BuildContext context) {
@@ -35,15 +38,96 @@ return SingleChildScrollView(
     ],
         ),
       ),
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40),
-        child: Text(
-          "Indent Type",
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-      ),
 
+      FormsHeadText("Indent Type"),
       DropdownForm(),
+
+      Padding(padding: EdgeInsets.all(10)),
+
+      FormsHeadText("Indent Date"),
+      DateTextFormField(),
+
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          height: 378,
+          width: 340,
+          decoration: BoxDecoration(
+            color: PrimaryColor9,
+            borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(padding: EdgeInsets.all(10)),
+
+                FormsHeadText("Item "),
+                DropdownForm(),
+
+                Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+
+                FormsHeadText("Request Qty. "),
+
+                Row(
+                  children:[
+                    Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Container(
+                      height: 50,
+                      padding: padding1,
+                      decoration: decoration1(),
+                      child: SizedBox(
+                        width: 130,
+                        child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          style: simpleTextStyle7(),
+                        ),
+                      ),
+                    ),
+                  ),
+                    SizedBox(
+                      width: 50,
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                          hint: Text(" "),
+                          dropdownColor: PrimaryColor3,
+                          icon: const Icon(Icons.keyboard_arrow_down_sharp),
+                          iconSize: 20,
+                          isExpanded: true,
+                          iconEnabledColor: PrimaryColor4,
+                          style: const TextStyle(color: PrimaryColor2, fontSize: 12),
+                          value: valueChoose,
+                          items: <DropdownMenuItem<int>>[
+                            new DropdownMenuItem(
+                              child: new Text('\tTon'),
+                              value: 0,
+                            ),
+                            new DropdownMenuItem(
+                              child: new Text('\tKG'),
+                              value: 4,
+                            ),
+                          ],
+                          onChanged: (Value) {
+                            setState(() {
+                              valueChoose = Value;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
+
+            ),
+
+          ),
+        ),
+
+
+
+
 
 
 
