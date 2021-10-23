27691f541711a66469f1de3b72530a106fa07a96
jatinkesharwani <jatinkesharwani360@gmail.com>
2021-10-23 17:21:09 +0530

    refactor: padding

--- lib/ui/pages/Staff UI/widgets/form_text.dart
+++ lib/ui/pages/Staff UI/widgets/form_text.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Padding FormsHeadText(String text){
   return Padding(
-    padding:EdgeInsets.symmetric(horizontal:40,vertical: 10),
+    padding:EdgeInsets.symmetric(horizontal:40),
     child: Text(
       text,
       style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),
