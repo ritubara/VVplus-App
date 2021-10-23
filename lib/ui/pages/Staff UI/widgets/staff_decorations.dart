8ba6cdf1b29d5c1a02e494162f915e7304f09a08
Roshan Mathews <roshanmathewsofficial@gmail.com>
2021-10-23 16:07:22 +0530

    refactor: add widget

---
+++ lib/ui/pages/Staff UI/widgets/staff_decorations.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:vvplus_app/ui/widgets/constants/colors.dart';
+
+
+BoxDecoration DecorationForms() {
+  return BoxDecoration(
+    color: PrimaryColor8,
+    borderRadius: BorderRadius.circular(5),
+    border: Border.all(color: PrimaryColor9,width: 1),
+
+  );
+}
---
+++ lib/ui/widgets/Utilities/raisedbutton_text.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+RaisedButtonText(String S){
+  return Text(
+    S,
+    style: TextStyle(color: Colors.blueAccent, fontSize: 14, fontWeight: FontWeight.bold),
+  );
+}
