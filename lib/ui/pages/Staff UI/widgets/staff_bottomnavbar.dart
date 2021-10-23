7f91ebabf50cd12fcf912b92e844287b7a1cda67
Roshan Mathews <roshanmathewsofficial@gmail.com>
2021-10-23 11:58:35 +0530

    chore: add icon

--- lib/ui/pages/Staff UI/widgets/staff_bottomnavbar.dart
+++ lib/ui/pages/Staff UI/widgets/staff_bottomnavbar.dart
@@ -32,15 +32,15 @@ class _MyBottomNavbarStaff extends State<BottomNavBarStaff>{
             title: Text('', style: TextStyle(color: PrimaryColor2))),
         BottomNavigationBarItem(
             icon: Icon(Icons.notifications,color: PrimaryColor3,),
-            activeIcon: Icon(Icons.home,color: PrimaryColor5,),
+            activeIcon: Icon(Icons.notifications,color: PrimaryColor5,),
             title: Text('', style: TextStyle(color: PrimaryColor2))),
         BottomNavigationBarItem(
             icon: Icon(Icons.check,size: 30,color: PrimaryColor3,),
-            activeIcon: Icon(Icons.home,color: PrimaryColor5,),
+            activeIcon: Icon(Icons.check,color: PrimaryColor5,),
             title: Text('', style: TextStyle(color: PrimaryColor2))),
         BottomNavigationBarItem(
             icon: Icon(Icons.account_circle,color: PrimaryColor3,),
-            activeIcon: Icon(Icons.home,color: PrimaryColor5,),
+            activeIcon: Icon(Icons.account_circle,color: PrimaryColor5,),
             title: Text('', style: TextStyle(color: PrimaryColor2))),
       ],
       currentIndex: _selectedIndex,
