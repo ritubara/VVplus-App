6c1ffea006412761fef5e83dd91546eb383ccdaa
Roshan Mathews <roshanmathewsofficial@gmail.com>
2021-10-29 19:02:51 +0530

    refactor : add button

--- lib/ui/pages/Staff UI/widgets/form_text.dart
+++ lib/ui/pages/Staff UI/widgets/form_text.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:vvplus_app/ui/widgets/Utilities/raisedbutton_text.dart';
 
 Padding FormsHeadText(String text){
   return Padding(
     padding:EdgeInsets.symmetric(horizontal:40),
     child: Text(
       text,
-      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),
+      style: TextStyle(fontWeight: FontWeight.w600,fontSize: 16),
     ),
   );
+}
 
+Padding ClearButton() {
+  return Padding(
+    padding: EdgeInsets.symmetric(vertical: 10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        RaisedButton(
+          onPressed: () {},
+          elevation: 0.0,
+          color: Colors.white,
+          child: RaisedButtonText("Clear all"),
+        ),
+      ],
+    ),
+  );
 }
