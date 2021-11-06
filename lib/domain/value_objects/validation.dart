16c25f69b4582ae0ad205f8ce8d6564700cebb26
jatinkesharwani <jatinkesharwani360@gmail.com>
2021-11-06 16:33:55 +0530

    chore: add validation for phone and otp using bloc

--- lib/domain/value_objects/validation.dart
+++ lib/domain/value_objects/validation.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 
 String validateMobile(String value) {
   String patttern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
@@ -25,7 +26,8 @@ String validateEmail(String value) {
   RegExp regex = RegExp(pattern);
   if (!regex.hasMatch(value)) {
     return 'Enter Valid Email';
-  } else {
+  }
+   else {
     return null;
   }
 }
@@ -39,4 +41,33 @@ String validateOTP(String value) {
   } else {
     return null;
   }
+}
+
+
+
+mixin Validators{
+
+  var phoneValidator = StreamTransformer<String,String>.fromHandlers(
+
+      handleData: (phone,sink){
+        bool phoneValid = RegExp(r'(^(?:[+0]9)?[0-9]{10,12}$)').hasMatch(phone);
+        if(phoneValid){
+          sink.add(phone);
+        }
+        else{
+          sink.addError("Mobile Number is not valid");
+        }
+      }
+  );
+
+  var otpValidator = StreamTransformer<String,String>.fromHandlers(
+      handleData: (otp,sink){
+        bool phoneValid = RegExp(r'(^(?:[+0]9)?[0-9]{5,10}$)').hasMatch(otp);
+        if(phoneValid){
+          sink.add(otp);
+        }else{
+          sink.addError("otp length should be greater than 4 and less than 11.");
+        }
+      }
+  );
 }
