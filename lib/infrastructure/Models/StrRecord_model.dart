8f1568f6172174e4f681b258680d3a7d1ebcfc90
jatinkesharwani <jatinkesharwani360@gmail.com>
2021-12-03 17:31:47 +0530

    refactor: json data

--- lib/infrastructure/Models/StrRecord_model.dart
+++ lib/infrastructure/Models/StrRecord_model.dart
@@ -9,84 +9,23 @@ List<StrRecord> StrRecordFromJson(String str) =>
 String StrRecordToJson(List<StrRecord> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-List<StrIndGrid> StrIndGridFromJson(String str) =>
-    List<StrIndGrid>.from(json.decode(str).map((x) => StrIndGrid.fromJson(x)));
-
-String StrIndGridToJson(List<StrRecord> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class StrRecord {
   StrRecord({
-    this.strIndTypeCode,
-    this.strSiteCode,
-    this.strIndNo,
-    this.strIndDate,
-    this.strDepartmentCode,
-    this.strIndentorCode,
-    this.strPreparedByCode,
-    this.strIndGrid,
+    this.strSubCode,
+    this.strName,
   });
 
-  String strIndTypeCode;
-  String strSiteCode;
-  String strIndNo;
-  String strIndDate;
-  String strDepartmentCode;
-  String strIndentorCode;
-  String strPreparedByCode;
-  List<StrIndGrid> strIndGrid;
+  String strSubCode;
+  String strName;
 
   factory StrRecord.fromJson(Map<String, dynamic> json) => StrRecord(
-    strIndTypeCode: json["StrIndTypeCode"],
-    strSiteCode: json["StrSiteCode"],
-    strIndNo: json["StrIndNo"],
-    strIndDate: json["StrIndDate"],
-    strDepartmentCode: json["StrDepartmentCode"],
-    strIndentorCode: json["StrIndentorCode"],
-    strPreparedByCode: json["StrPreparedByCode"],
-    strIndGrid: List<StrIndGrid>.from(json["StrIndGrid"].map((x) => StrIndGrid.fromJson(x))),
+    strSubCode: json["StrSubCode"],
+    strName: json["StrName"],
   );
 
   Map<String, dynamic> toJson() => {
-    "StrIndTypeCode": strIndTypeCode,
-    "StrSiteCode": strSiteCode,
-    "StrIndNo": strIndNo,
-    "StrIndDate": strIndDate,
-    "StrDepartmentCode": strDepartmentCode,
-    "StrIndentorCode": strIndentorCode,
-    "StrPreparedByCode": strPreparedByCode,
-    "StrIndGrid": List<dynamic>.from(strIndGrid.map((x) => x.toJson())),
-  };
-}
-
-class StrIndGrid {
-  StrIndGrid({
-    this.strItemCode,
-    this.dblQuantity,
-    this.strCostCenterCode,
-    this.strRequiredDate,
-    this.strRemark,
-  });
-
-  String strItemCode;
-  String dblQuantity;
-  String strCostCenterCode;
-  String strRequiredDate;
-  String strRemark;
-
-  factory StrIndGrid.fromJson(Map<String, dynamic> json) => StrIndGrid(
-    strItemCode: json["StrItemCode"],
-    dblQuantity: json["DblQuantity"],
-    strCostCenterCode: json["StrCostCenterCode"],
-    strRequiredDate: json["StrRequiredDate"],
-    strRemark: json["StrRemark"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "StrItemCode": strItemCode,
-    "DblQuantity": dblQuantity,
-    "StrCostCenterCode": strCostCenterCode,
-    "StrRequiredDate": strRequiredDate,
-    "StrRemark": strRemark,
+    "StrSubCode": strSubCode,
+    "StrName": strName,
   };
 }
