09056746f6ee37157f6754c7b954178de7d6f0ce
rosha <roshanmathewsofficial@gmail.com>
2021-12-11 22:09:34 +0530

    feat: fetch dropdown data from firestore

--- lib/ui/pages/Staff UI/widgets/Dropdown/indentor_name_dropdown.dart
+++ lib/ui/pages/Staff UI/widgets/Dropdown/indentor_name_dropdown.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:search_choices/search_choices.dart';
 import 'package:vvplus_app/Application/Bloc/Dropdown_Bloc/indentor_name_dropdown_bloc.dart';
+import 'package:vvplus_app/infrastructure/Models/indentor_name_model.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/staff_containers.dart';
 import 'package:vvplus_app/ui/widgets/constants/size.dart';
 
@@ -13,6 +14,67 @@ class IndentorNameDropdown extends StatefulWidget {
 }
 
 class _IndentorNameDropdownState extends State<IndentorNameDropdown> {
+  IndentorNameDropdownBloc _dropdownBloc;
+
+  @override
+  void initState() {
+    _dropdownBloc = IndentorNameDropdownBloc();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding1,
+      child: Container(
+        height: 50, width: 343,
+        decoration: DecorationForms(),
+        child: FutureBuilder<List<IndentorName>>(
+            future: _dropdownBloc.indentorNameDropdownData,
+            builder: (context, snapshot) {
+              return StreamBuilder<IndentorName>(
+                  stream: _dropdownBloc.selectedState,
+                  builder: (context, item) {
+                    return SearchChoices<IndentorName>.single(
+                      icon: const Icon(Icons.keyboard_arrow_down_sharp),
+                      underline: "",
+                      padding: 1,
+                      isExpanded: true,
+                      hint: "Search here",
+                      value: item.data,
+                      displayClearIcon: false,
+                      onChanged: _dropdownBloc.selectedStateEvent,
+                      items: snapshot?.data
+                          ?.map<DropdownMenuItem<IndentorName>>((e) {
+                        return DropdownMenuItem<IndentorName>(
+                          value: e,
+                          child: Text(e.strName),
+                        );
+                      })?.toList() ??[],
+                    );
+                  }
+              );
+            }
+        ),
+      ),
+    );
+  }
+}
+
+class IndentorNameDropdownFirestore extends StatefulWidget {
+  const IndentorNameDropdownFirestore({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _IndentorNameDropdownFirestore();
+}
+
+
+class _IndentorNameDropdownFirestore extends State<IndentorNameDropdownFirestore> {
   IndentorNameDropdownBloc _dropdownBloc;
 
   @override
@@ -87,3 +149,4 @@ class _IndentorNameDropdownState extends State<IndentorNameDropdown> {
     );
   }
 }
+
