663c8a8e28436e04ee34b96452652a810b53de8b
rosha <roshanmathewsofficial@gmail.com>
2022-01-04 21:48:59 +0530

    chore: add demo data approval,deny and wait

--- lib/ui/pages/Staff UI/screens/purchase page/material_request_approval/forms_container_data.dart
+++ lib/ui/pages/Staff UI/screens/purchase page/material_request_approval/forms_container_data.dart
@@ -1,8 +1,15 @@
 // ignore_for_file: avoid_print
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:vvplus_app/data_source/api/api_services.dart';
+import 'package:vvplus_app/domain/common/common_text.dart';
+import 'package:vvplus_app/domain/common/snackbar_widget.dart';
+import 'package:vvplus_app/infrastructure/Models/indentor_name_model.dart';
+import 'package:vvplus_app/infrastructure/Models/item_cost_center_model.dart';
+import 'package:vvplus_app/infrastructure/Models/item_current_status_model.dart';
 import 'package:vvplus_app/ui/pages/Staff%20UI/widgets/staff_text_style.dart';
 import 'package:vvplus_app/ui/widgets/constants/assets.dart';
 import 'package:vvplus_app/ui/widgets/constants/colors.dart';
@@ -21,12 +28,52 @@ class _MaterialApprovalPageContainerDataState
     extends State<MaterialApprovalPageContainerData> {
   List data;
   bool isActive = false;
+  bool pressed = false;
+  bool post = true;
+
+  TextEditingController intendDateInput = TextEditingController();
+  TextEditingController reqDateInput = TextEditingController();
+  TextEditingController indentType = TextEditingController();
+  TextEditingController item = TextEditingController();
+  TextEditingController reqQty = TextEditingController(text:'0');
+  TextEditingController rate = TextEditingController();
+  TextEditingController costCenter = TextEditingController();
+  TextEditingController remarks = TextEditingController();
+
+
+  IndentorName selectIndentName;
+  ItemCurrentStatus selectItemCurrentStatus;
+  ItemCostCenter selectItemCostCenter;
 
   fetchData1() async {
     http.Response response = await http.get(Uri.parse(ApiService.mockDataPostMaterialRequestEntryURL));
     setState(() {
       data = json.decode(response.body);
     });
+  }
+
+  Future<dynamic> sendData() async{
+    try {
+      await http.post(Uri.parse("https://vv-plus-app-default-rtdb.firebaseio.com/PostDataRecord/0/ApprovedMaterialData/0.json"),
+          body: json.encode({
+            "IndentSubCode":selectIndentName.strSubCode,
+            "IntendDate":intendDateInput.text,
+            "ItemName":selectItemCurrentStatus.strItemName,
+            "ReqQty":reqQty.text,
+            "ItemUnit":selectItemCurrentStatus.strUnit,
+            "Rate":selectItemCurrentStatus.dblQty,
+            "ItemSubCode":selectItemCostCenter.strSubCode,
+            "ReqDate":reqDateInput.text,
+            "Remarks":remarks.text
+          }));
+      Scaffold.of(context).showSnackBar(snackBar(sendDataText));
+    } on SocketException {
+      Scaffold.of(context).showSnackBar(snackBar(socketExceptionText));
+    } on HttpException {
+      Scaffold.of(context).showSnackBar(snackBar(httpExceptionText));
+    } on FormatException {
+      Scaffold.of(context).showSnackBar(snackBar(formatExceptionText));
+    }
   }
 
   @override
@@ -41,18 +88,20 @@ class _MaterialApprovalPageContainerDataState
         ? const Center(
             child: CircularProgressIndicator(),
           )
-        : SingleChildScrollView(
+        : post?SingleChildScrollView(
             physics: const ScrollPhysics(),
             child: Column(children: [
               ListView.builder(
                 physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
+                itemCount: data == null ? 0 : data.length,
                 itemBuilder: (context, index) {
                   return GestureDetector(
                     onTap: () {
                       setState(() {
                         isActive = true;
                         isActive = isActive;
+                        pressed = true;
                       });
                       print("no");
                     },
@@ -64,7 +113,7 @@ class _MaterialApprovalPageContainerDataState
                         width: SizeConfig.getWidth(context),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(5.0),
-                          color: primaryColor3,
+                          color: pressed?Colors.blue[200]:primaryColor3,
                           boxShadow: const [
                             BoxShadow(
                               color: primaryColor5,
@@ -211,7 +260,7 @@ class _MaterialApprovalPageContainerDataState
                     ),
                   );
                 },
-                itemCount: data == null ? 0 : data.length,
+
               ),
               sizedbox1,
               SizedBox(
@@ -224,7 +273,29 @@ class _MaterialApprovalPageContainerDataState
                       primary: primaryColor1,
                       onSurface: primaryColor1,
                     ),
-                    onPressed: isActive ? () {} : null,
+                    onPressed: isActive ? () {
+                      //sendData();
+                      showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) =>AlertDialog(
+                        content: Text("Do you want to approve the selected Datas"),
+                        actions: [
+                          FlatButton(
+                              onPressed: (){
+                                setState(() {
+                                  post = false;
+                                  Navigator.pop(context, 'Cancel');
+                                });
+                              },
+                              child: Text("yes")),
+                          FlatButton(
+                              onPressed: (){
+                                Navigator.pop(context, 'Cancel');
+                              },
+                              child: Text("No")),
+                        ],
+                      ),);
+                    } : null,
                     child: const Text("Approve")),
               ),
               sizedbox1,
@@ -238,7 +309,41 @@ class _MaterialApprovalPageContainerDataState
                       primary: primaryColor6,
                       onSurface: primaryColor6,
                     ),
-                    onPressed: isActive ? () {} : null,
+                    onPressed: isActive ? () {
+                      showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) =>AlertDialog(
+                          content: Text("Do you want to delete the selected Datas"),
+                          actions: [
+                            FlatButton(
+                                onPressed: (){
+                                  setState(() {
+                                    post = false;
+                                    Navigator.pop(context, 'Cancel');
+                                  });
+                                  final snackBar = SnackBar(
+                                    content: const Text('Data delected succesfully'),
+                                    action: SnackBarAction(
+                                      label: 'Undo',
+                                      onPressed: () {
+                                        // Some code to undo the change.
+                                      },
+                                    ),
+                                  );
+
+                                  // Find the ScaffoldMessenger in the widget tree
+                                  // and use it to show a SnackBar.
+                                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                },
+                                child: Text("yes")),
+                            FlatButton(
+                                onPressed: (){
+                                  Navigator.pop(context, 'Cancel');
+                                },
+                                child: Text("No")),
+                          ],
+                        ),);
+                    } : null,
                     child: const Text("Deny")),
               ),
               sizedbox1,
@@ -252,11 +357,32 @@ class _MaterialApprovalPageContainerDataState
                       primary: primaryColor5,
                       onSurface: primaryColor5,
                     ),
-                    onPressed: isActive ? () {} : null,
+                    onPressed: isActive ? () {
+                      showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) =>AlertDialog(
+                          content: Text("Datas are set to wait"),
+                          actions: [
+                            FlatButton(
+                                onPressed: (){
+                                  setState(() {
+                                    pressed = false;
+                                    Navigator.pop(context, 'Cancel');
+                                  });
+                                },
+                                child: Text("Ok")),
+
+                          ],
+                        ),);
+                    } : null,
                     child: const Text("Wait")),
               ),
               sizedbox1,
             ]),
-          );
+          ):SizedBox(
+      height: 50,
+        child: Center(
+            child: Text("No records to show")));
   }
+
 }
